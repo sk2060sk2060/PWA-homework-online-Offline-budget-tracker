ec14d803f6aee0cb7924dfa51adb144ec692b376
sk <sk2060sk2060@gmail.com>
2020-05-04 00:06:47 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// If deployed, use the deployed database. Otherwise use the local budget database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api.js"));
